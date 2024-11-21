4aea61ae44cd060532d9dfe66c0881f2fd6bc470
Lucas Silva <lucasilvabr@gmail.com>
2024-11-20 22:49:21 -0300

    ajuste-codigo
    Por enquanto funcionando como quotes para ser atualizado.

--- script.js
+++ script.js
@@ -1,95 +1,104 @@
-const projectName = 'random-recipe-machine';
-let recipesData;
+/* eslint-disable max-len */
+// eslint-disable-next-line no-unused-vars
+const projectName = 'random-quote-machine';
+let quotesData;
+
+/*
+  Code by Gabriel Nunes
+  Modified by Todd Chaffee to use Camper gist for JSON Quote data.
+*/
 
 var colors = [
-    '#16a085',
-    '#27ae60',
-    '#2c3e50',
-    '#f39c12',
-    '#e74c3c',
-    '#9b59b6',
-    '#FB6964',
-    '#342224',
-    '#472E32',
-    '#BDBB99',
-    '#77B1A9',
-    '#73A857'
+  '#16a085',
+  '#27ae60',
+  '#2c3e50',
+  '#f39c12',
+  '#e74c3c',
+  '#9b59b6',
+  '#FB6964',
+  '#342224',
+  '#472E32',
+  '#BDBB99',
+  '#77B1A9',
+  '#73A857'
 ];
+var currentQuote = '',
+  currentAuthor = '';
 
-var currentId= '',
-    currentRecipe = '';
-
-function getRecipes(){
-    return $.ajax({
-        headers: {
-            Accept: 'application/json'
-        },
-        url:
-        'https://raw.githubusercontent.com/adrianosferreira/afrodite.json/refs/heads/master/afrodite.json',
-        success: function(jsonRecipes){
-            if (typeof jsonRecipes === 'string'){
-                recipesData = JSON.parse(jsonRecipes);
-                console.log('recipesData');
-                console.log(recipesData);
-            }
-        }
-    });
+function getQuotes() {
+  return $.ajax({
+    headers: {
+      Accept: 'application/json'
+    },
+    url:
+      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+    success: function (jsonQuotes) {
+      if (typeof jsonQuotes === 'string') {
+        quotesData = JSON.parse(jsonQuotes);
+        console.log('quotesData');
+        console.log(quotesData);
+      }
+    }
+  });
 }
 
-function getRandomRecipe(){
-    return recipesData.Id[
-        Math.floor(Math.random() * recipesData.id.length)
-    ];
+function getRandomQuote() {
+  return quotesData.quotes[
+    Math.floor(Math.random() * quotesData.quotes.length)
+  ];
 }
 
-function getRecipe(){
-    let randomRecipe = getRandomRecipe();
+function getQuote() {
+  let randomQuote = getRandomQuote();
 
-    currentId = randomRecipe.id;
-    currentRecipe = randomRecipe.nome;
+  currentQuote = randomQuote.quote;
+  currentAuthor = randomQuote.author;
 
-    $('#tweet-receita').attr(
-        'href',
-        'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
-        encodeURIComponent('"' + currentId + '"' + currentRecipe)
-    );
+  $('#tweet-quote').attr(
+    'href',
+    'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+      encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+  );
 
-    $('#facebook-receita').attr(
-        'href',
-        'https://www.facebook.com/sharer/sharer.php?u=' +
-        encodeURIComponent('"' + currentId + '"' + currentRecipe)
-    );
+  $('#tumblr-quote').attr(
+    'href',
+    'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=' +
+      encodeURIComponent(currentAuthor) +
+      '&content=' +
+      encodeURIComponent(currentQuote) +
+      '&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button'
+  );
 
-    $('.quote-text').animate({opacity: 0}, 500, function(){
-        $(this).aniamte({opacity: 1}, 500);
-        $('#text').text(randomRecipe.nome);
-    });
+  $('.quote-text').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $('#text').text(randomQuote.quote);
+  });
 
-    $('.quote-receita').animate({opacity: 0}, 500, function(){
-        $(this).aniamte({opacity: 1}, 500);
-        $('#receita').text(randomRecipe.conteudo);
-    });
+  $('.quote-author').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $('#author').html(randomQuote.author);
+  });
 
-    var color = Math.floor(Math.random() * colors.length);
-    $('html body').animate(
-        {
-            backgroundColor: color[color],
-            color: colors[color]
-        },
-        1000
-    );
-    $('.button').animate(
-        {
-            backgroundColor: colors[color]
-        },
-        1000
-    );
+  var color = Math.floor(Math.random() * colors.length);
+  $('html body').animate(
+    {
+      backgroundColor: colors[color],
+      color: colors[color]
+    },
+    1000
+  );
+  $('.button').animate(
+    {
+      backgroundColor: colors[color]
+    },
+    1000
+  );
 }
 
 $(document).ready(function () {
-    getRecipes().then(() => {
-        getRecipe();
-    });
+  getQuotes().then(() => {
+    getQuote();
+  });
 
-    $('#new-quote').on('click', getRecipe);
+  $('#new-quote').on('click', getQuote);
 });
